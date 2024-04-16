0c354f9fc3d5453886a71d6d1825efa484a3dfc0
valentinbassom <100703823+valentinbassom@users.noreply.github.com>
2024-04-16 14:41:20 +0200

    Delete omp_hello_world.c

--- omp_hello_world.c
+++ omp_hello_world.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
 
-int main(int argc, char *argv[]){
-
-    if(argc > 1){
-        omp_set_num_threads(atoi(argv[1]));
-    }
-    #pragma omp parallel
-    {
-        int id = omp_get_thread_num();
-        printf("Hello World from Thread %d!\n",id);
-    }
-
-    return 0;
-}
