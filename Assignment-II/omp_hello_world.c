91301c75b4f10a753e636491a617300c17b92d31
valentinbassom <valentinbassom@gmail.com>
2024-04-16 17:11:27 +0200

    Change parser

--- Assignment-II/omp_hello_world.c
+++ Assignment-II/omp_hello_world.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <omp.h>
 
 int main(int argc, char *argv[]){
 
-    if(argc > 1){
-        omp_set_num_threads(atoi(argv[1]));
+    if(argc > 1 && strcmp(argv[1],"-n_threads") == 0){
+        omp_set_num_threads(atoi(argv[2]));
     }
     #pragma omp parallel
     {
